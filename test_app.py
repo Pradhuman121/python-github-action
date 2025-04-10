639dd21d038584bee0b1fd632fb1586f30a91981
Pradhuman Singh <pradhuman.singh@involead.com>
2025-04-10 13:22:36 +0530

    check

--- test_app.py
+++ test_app.py
@@ -1,7 +1,10 @@
+import pytest
 from app import add, subtract
 
+
 def test_add():
-    assert add(3, 4) == 7
+    assert add(2, 3) == 5
+
 
 def test_subtract():
-    assert subtract(10, 3) == 7
+    assert subtract(5, 2) == 3
